9134efb2575dab6d1a82e11193d37f0ff0dcc062
aniksa <aniksa@gmail.com>
2018-03-27 14:23:35 +0300

    Update script.js

--- calc/script.js
+++ calc/script.js
@@ -1,49 +1,43 @@
 function Form(el){
     let elem = el;
-    let resultEl = elem.getElementsByClassName('result')[0];
+    let resultEl = elem.getElementsByClassName('result')[0]; //нашли блок, в который будем выводить результат
     let inputs = [];
-    this.getInputs = function(){
-        let inputElems = elem.getElementsByTagName('input');
-        for (let i of inputElems) {
-            let inp;
-			let type = i.getAttribute("type");
-            if ( type === "radio") {
-                inp = new Radio(i);
-            } else if (type === "checkbox") {
-				inp = new Checkbox(i);
+	
+    //вспомогательные (приватные) функции	
+	function pushElemsByTag(tag){ //добавляем элементы формы по тегу к inputs
+		let inp;
+		let inputElems = elem.getElementsByTagName(tag); //получим все элементы нужного типа	
+        for (let i of inputElems) {//в зависимости от типа элемента формы воспользуемся разными конструкторами
+			switch(i.getAttribute("type")){
+				case "radio": inp = new Radio(i); break;
+				case "checkbox": inp = new Checkbox(i); break;
+				default: inp = new Input(i);
 			}
-			else {	
-				inp = new Input(i);
-			}
-            inputs.push(inp);
+			inputs.push(inp); //добавляем элемент в массив
         }
-        inputElems = elem.getElementsByTagName('select');
-        for (let i of inputElems) {
-            let inp = new Input(i);
-            inputs.push(inp);
-        }
-    };
-    function total(){
+	}
+    function total(){ //складываем value всех элементов 
         let sum = 0;
         for (let inp of inputs){
             sum += Number(inp.value());
         }
         return sum;
     };
-	elem.onclick = function(event){
-		let target = event.target;
+	function showResult(){
+		resultEl.innerHTML = ""+total();
+	}
+	
+	this.init = function(){//заполним массив inputs
+		pushElemsByTag('input'); //получим все input(s)	
+		pushElemsByTag('select'); //получим все select(s)		
+    };
+	elem.onclick = function(event){ //обработка клика по форме - слушаем клик по всей html-форме
+		let target = event.target; //куда кликнули, если на input или select - выводим результат
 		if (target.closest('input')|| target.closest('select'))
-			//calculateTotal(target);
-			resultEl.innerHTML = ""+total();
-			console.log(target);
-	};
-	this.getInputValue = function(i){
-		return inputs[i].value();
+			showResult();
+			console.log(target); //информация в консоль для отладки
 	};
 }
-
-let form = new Form(document.forms["cakeform"]);
-form.getInputs();
 
 function Input(el){
     let elem = el;
@@ -56,7 +50,7 @@ function Input(el){
 function Radio(el){
     Input.call(this);
     function getValue(){
-		if (el.checked)
+		if (el.checked)//если элемент выбран, то берем его значение, если нет - возвращаем 0
 			return el.value;
 		return 0;
     }
@@ -72,3 +66,6 @@ function Checkbox(el){
     }
     this.value = getValue;
 }
+
+let form = new Form(document.forms["cakeform"]);
+form.init();
